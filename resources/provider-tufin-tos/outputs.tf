1cccbe0a9675d32eef6193f0a46ab8de60bcb7ca
Chris Roth <me@chrisroth.io>
2022-02-16 16:20:26 +0100

    resources

--- resources/provider-tufin-tos/outputs.tf
+++ resources/provider-tufin-tos/outputs.tf
@@ -7,6 +7,14 @@ output "tufin_servers_mars_1_servers" {
   value       = module.servers.mars_1_servers
   description = "servers by name"
 }
+output "tufin_servers_pluto_1_name" {
+  value       = module.servers.pluto_1_name
+  description = "server name"
+}
+output "tufin_servers_pluto_1_servers" {
+  value       = module.servers.pluto_1_servers
+  description = "servers by name"
+}
 
 # module services
 output "tufin_services_by_name" {
---
+++ resources/provider-tufin-tos/servers/pluto.tf
@@ -0,0 +1,35 @@
+resource "tufin_server" "pluto_1" {
+  domain = var.domain
+  app    = var.app
+
+  name           = "PLUTO_1"
+  group_id       = 1
+  ip             = "1.2.3.4"
+  security_level = ""
+  comment        = "Test Server PLUTO 1 .. Created by Tufin Terraform Provider"
+  tags = merge(
+    var.default_tags,
+    {
+      network_object_SA = format("%s", "PLUTO_1")
+    })
+}
+output "pluto_1" {
+  value = tufin_server.pluto_1
+}
+
+data "tufin_servers" "pluto_1_name" {
+  name = "pluto_1"
+
+  domain = var.domain
+  app    = var.app
+}
+
+output "pluto_1_name" {
+  value       = data.tufin_servers.pluto_1_name.name
+  description = "servers by name, name"
+}
+output "pluto_1_servers" {
+  value       = data.tufin_servers.pluto_1_name.servers
+  description = "servers by name, servers"
+}
+

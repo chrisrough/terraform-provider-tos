aea2a171ec3f9e5a31031710632a9ae56c3693ec
TSGRURE1 <reto.rutishauser@swisscom.com>
2022-02-28 11:26:39 +0100

    add lambda

--- resources/provider-tufin-tos/usecases/lambda.tf
+++ resources/provider-tufin-tos/usecases/lambda.tf
@@ -1,12 +1,22 @@
 #get Info from lambda function
 
-resource "tufin_subnet" "Subnet_lambda" {
-  domain = var.domain
-  app = var.app
-  name= "Sub_lambda"
-  group_id= 1
-  ip= "10.10.10.0/24"
-  security_level = ""
-  comment= "Test Subnet lambda 1 .. Created by Tufin Terraform Provider"
-  tags = merge( var.default_tags, { network_object_SA = format("%s", "lambda_1") })
+# get Lambda Function
+data "aws_lambda_function" "application" {
+  function_name = "application"
 }
+# show lambda function
+output "lambda" {
+  value = aws_lambda_function.application
+}
+
+# Create lambda IP Range at tufin
+#resource "tufin_subnet" "Subnet_lambda" {
+#  domain = var.domain
+#  app = var.app
+#  name= "Sub_lambda"
+#  group_id= 1
+#  ip= "10.10.10.0/24"
+#  security_level = ""
+#  comment= "Test Subnet lambda 1 .. Created by Tufin Terraform Provider"
+#  tags = merge( var.default_tags, { network_object_SA = format("%s", "lambda_1") })
+#}

639c2e824d119468ef1bbaf3e70a105d26e62ef8
TSGRURE1 <reto.rutishauser@swisscom.com>
2022-02-28 15:46:51 +0100

    bugfix

--- resources/provider-tufin-tos/usecases/var.tf
+++ resources/provider-tufin-tos/usecases/var.tf
@@ -7,3 +7,8 @@ variable "app" {
   type        = string
   description = "Tufin App (within Domain)"
 }
+# region EU-Central-1, Frankfurt
+variable "aws_region" {
+  description = "sandbox.io"
+  default = "eu-central-1"
+}

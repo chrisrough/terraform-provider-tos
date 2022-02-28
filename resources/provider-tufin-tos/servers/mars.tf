3bb017f9ab7b9ef8c5a3dda9a23ecc060ec7b654
TSGRURE1 <reto.rutishauser@swisscom.com>
2022-02-28 15:35:46 +0100

    bugfix

--- resources/provider-tufin-tos/servers/mars.tf
+++ resources/provider-tufin-tos/servers/mars.tf
@@ -4,7 +4,6 @@ resource "tos_server" "mars_1" {
   name           = "MARS_1"
   group_id       = 1
   ip             = "1.2.3.4"
-  security_level = ""
   comment        = "Test Server MARS 1 .. Created by Tufin Terraform Provider"
   tags = merge(
     var.default_tags,
@@ -16,7 +15,7 @@ output "mars_1" {
   value = tos_server.mars_1
 }
 
-data "tos_servers" "mars_1_name" {
+data "tos_server" "mars_1_name" {
   name = "MARS_1"
 
   domain = var.domain
@@ -38,7 +37,6 @@ resource "tos_server" "Subnet_mars" {
   name= "SUB_MARS_1"
   group_id= 1
   ip= "10.10.10.0/24"
-  security_level = ""
   comment= "Test Subnet MARS 1 .. Created by Tufin Terraform Provider"
   tags = merge( var.default_tags, { network_object_SA = format("%s", "SUB_MARS_1") })
 }

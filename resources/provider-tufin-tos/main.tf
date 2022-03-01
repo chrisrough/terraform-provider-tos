56dd02378838e010646b157bf628d29bb449278e
TSGRURE1 <reto.rutishauser@swisscom.com>
2022-03-01 06:56:20 +0100

    bugfix

--- resources/provider-tufin-tos/main.tf
+++ resources/provider-tufin-tos/main.tf
@@ -1,3 +1,5 @@
+
+
 module "servers" {
   source = "./servers"
 
--- resources/provider-tufin-tos/usecases/provider.tf
+++ resources/provider-tufin-tos/usecases/provider.tf
@@ -7,6 +7,17 @@ terraform {
   }
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
 # aws region
 provider "aws" {
   region = var.aws_region

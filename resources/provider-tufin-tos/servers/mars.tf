6057584cc1ffd0a04fdc6f0d84f257fcc33cfdcb
TSGRURE1 <reto.rutishauser@swisscom.com>
2022-03-02 13:42:06 +0100

    bugfix

--- resources/provider-tufin-tos/servers/mars.tf
+++ resources/provider-tufin-tos/servers/mars.tf
@@ -1,31 +1,31 @@
-resource "tos_server" "mars_2" {
-  domain = var.domain
-  app    = var.app
-  name           = "MARS_2"
-  group_id       = 1
-  ip             = "1.2.3.41"
-  comment        = "Test Server MARS 2 .. Created by Tufin Terraform Provider"
-  tags = merge(
-    var.default_tags,
-    {
-      network_object_SA = format("%s", "MARS_2")
-    })
-}
-output "mars_2" {
-  value = tos_server.mars_2
-}
+# resource "tos_server" "mars_2" {
+#   domain = var.domain
+ #  app    = var.app
+ #  name           = "MARS_2"
+#   group_id       = 1
+#   ip             = "1.2.3.41"
+#   comment        = "Test Server MARS 2 .. Created by Tufin Terraform Provider"
+#   tags = merge(
+#     var.default_tags,
+ #    {
+#       network_object_SA = format("%s", "MARS_2")
+#     })
+# }
+# output "mars_2" {
+#   value = tos_server.mars_2
+# }
 
-data "tos_networkobjects" "mars_2_name" {
-  name = "MARS_2"
+# data "tos_networkobjects" "mars_2_name" {
+#   name = "MARS_2"
 
-  domain = var.domain
-  app    = var.app
-}
+#   domain = var.domain
+#   app    = var.app
+# }
 
-output "mars_2_name" {
-  value       = data.tos_networkobjects.mars_2_name.name
-  description = "servers by name, name"
-}
+# output "mars_2_name" {
+#   value       = data.tos_networkobjects.mars_2_name.name
+#   description = "servers by name, name"
+# }
 
 
 resource "tos_server" "Subnet_mars" {

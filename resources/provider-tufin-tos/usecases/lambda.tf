95f11d295418e50018a3a25b6d6a5858493cca14
TSGRURE1 <reto.rutishauser@swisscom.com>
2022-03-03 11:19:33 +0100

    bugfix

--- resources/provider-tufin-tos/usecases/lambda.tf
+++ resources/provider-tufin-tos/usecases/lambda.tf
@@ -6,7 +6,7 @@ data "aws_lambda_function" "application" {
 }
 # show lambda function
 output "lambda" {
-  value = data.aws_lambda_function.application.vpc_config.subnet_ids[0]
+  value = data.aws_lambda_function.application.vpc_config.subnet_ids[1]
   #value = [for s in data.aws_lambda_function.application.vpc_config.subnet_ids : s]
 }
 

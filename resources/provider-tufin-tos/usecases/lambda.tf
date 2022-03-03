e13987a1f0ab18d4324e12b61b4cfb14dfd4695a
TSGRURE1 <reto.rutishauser@swisscom.com>
2022-03-03 10:55:26 +0100

    bugfix

--- resources/provider-tufin-tos/usecases/lambda.tf
+++ resources/provider-tufin-tos/usecases/lambda.tf
@@ -6,7 +6,8 @@ data "aws_lambda_function" "application" {
 }
 # show lambda function
 output "lambda" {
-  value = data.aws_lambda_function.application.vpc_config.subnet_ids[*]
+ # value = data.aws_lambda_function.application.vpc_config.subnet_ids[*]
+  value = [for s in data.aws_lambda_function.application.vpc_config.subnet_ids : s]
 }
 
 # Create lambda IP Range at tufin
